7390e4e1002a4b40a4622612c1dcf6821a10bbf5
Елфимов Алексей <elfimov@kg.ru>
2020-10-05 17:46:40 +0400

    Maven Wrapper

---
+++ src/main/java/com/hugo/courselab/AngularWebAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hugo.courselab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularWebAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularWebAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugo/courselab/AngularWebAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.courselab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularWebAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
